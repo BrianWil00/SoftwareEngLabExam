a8366cfdfde8b5bc2839f1060ca3865064ea72ef
Brian Williams <brianwil00@gmail.com>
2022-03-08 10:45:18 +0000

    EmployeeList Tested but failed

--- src/test/java/ie/gmit/EmployeeListTest.java
+++ src/test/java/ie/gmit/EmployeeListTest.java
@@ -1,4 +1,22 @@
 package ie.gmit;
 
+import org.junit.jupiter.api.Test;
+
 public class EmployeeListTest {
+
+    @Test
+    void testValidEmployeeList(){
+        employees.add(new Employee("Brian", "12345"));
+        new EmployeeList(employees);
+    }
+
+    @Test
+    void testAddEmployee(){
+        employees.addEmployee(new Employee("John Doe", "22222"));
+    }
+
+    @Test
+    void testDeleteEmployee(){
+        employees.deleteEmployee(new Employee("John Doe", "22222"));
+    }
 }
